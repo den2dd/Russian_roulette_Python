13da89d0a71cadf81da5fdfa3e0954defc3a4ced
den2dd <dd.rutikov@gmail.com>
2017-01-06 13:07:27 +0300

    Убраны линии во время перемещений пера. Увеличена скорость отрисовки окружностей.

--- Russian_roulette.py
+++ Russian_roulette.py
@@ -2,12 +2,15 @@ import turtle
 import random
 
 turtle.circle(20)
+turtle.speed(0)
 
 answer = ''
 while answer != 'N':
     answer = turtle.textinput("Нарисовать окружность", "Y/N")
     if answer == 'Y':
+        turtle.penup()
         turtle.goto(random.randrange(-300,300), random.randrange(-200,200))
+        turtle.pendown()
         turtle.circle(random.randrange(1,100))
     else:
         pass
