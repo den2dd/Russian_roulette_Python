24a58e93c5935750a083ec0637ca845e6ff99d64
den2dd <dd.rutikov@gmail.com>
2017-01-06 14:18:58 +0300

    Создана функция для рисования барабана

--- Russian_roulette.py
+++ Russian_roulette.py
@@ -2,6 +2,9 @@ import turtle
 import random
 import math
 
+PHI = 360 / 7
+R = 50
+
 def gotoxy(x,y):
     turtle.penup()
     turtle.goto(x, y)
@@ -15,22 +18,18 @@ def draw_circle(r, color):
 
 turtle.speed(0)
 
-gotoxy(0,0)
-turtle.circle(80)
-gotoxy(0,160)
-draw_circle(5, 'red')
+def draw_pistol():
+    gotoxy(0,0)
+    turtle.circle(80)
+    gotoxy(0,160)
+    draw_circle(5, 'red')
 
-phi = 360 / 7
-r = 50
+    for i in range(0,7):
+        phi_rad = PHI * i * math.pi / 180.0
+        gotoxy(math.sin(phi_rad)*R, math.cos(phi_rad)*R + 60)
+        draw_circle(22, 'white')
 
-for i in range(0,7):
-    phi_rad = phi * i * math.pi / 180.0
-    gotoxy(math.sin(phi_rad)*r, math.cos(phi_rad)*r + 60)
-    draw_circle(22, 'white')
-
-
-
-
+draw_pistol()
 
 answer = ''
 start = 0
@@ -39,12 +38,12 @@ while answer != 'N':
     if answer == 'Y':
 
         for i in range(start, random.randrange(7, 100)):
-            phi_rad = phi * i * math.pi / 180.0
-            gotoxy(math.sin(phi_rad) * r, math.cos(phi_rad) * r + 60)
+            phi_rad = PHI * i * math.pi / 180.0
+            gotoxy(math.sin(phi_rad) * R, math.cos(phi_rad) * R + 60)
             draw_circle(22, 'brown')
             draw_circle(22, 'white')
 
-        gotoxy(math.sin(phi_rad) * r, math.cos(phi_rad) * r + 60)
+        gotoxy(math.sin(phi_rad) * R, math.cos(phi_rad) * R + 60)
         draw_circle(22, 'brown')
 
         start = i % 7
