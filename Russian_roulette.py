52862a66004845501f2bd5099c1879e4365f0699
den2dd <dd.rutikov@gmail.com>
2017-01-06 13:03:57 +0300

    Следующие окружности теперь рисуются в рандомном месте в пределах координат х(-300,300) и у(-200,200)

--- Russian_roulette.py
+++ Russian_roulette.py
@@ -7,6 +7,7 @@ answer = ''
 while answer != 'N':
     answer = turtle.textinput("Нарисовать окружность", "Y/N")
     if answer == 'Y':
+        turtle.goto(random.randrange(-300,300), random.randrange(-200,200))
         turtle.circle(random.randrange(1,100))
     else:
         pass
